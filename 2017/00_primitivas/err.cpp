3499f68176e6750e19dd021131b1a9e97d8d5f4f
Oscar Calderon Moran <gorri0302@gmail.com>
2017-11-07 11:32:24 +0100

    our first program

---
+++ 2017/00_primitivas/err.cpp
@@ -0,0 +1,9 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+        fprintf(stderr, "stderr: Hola mundo.\n");
+	return EXIT_SUCCESS;
+}
---
+++ 2017/00_primitivas/javascript/javascript/main.js
@@ -0,0 +1,5 @@
+function main() {
+    var stdout = document.getElementById("stdout")
+    stdout.innerHTML  +=  "stdout: THE WORLD IS A VAMPIRE."
+    alert("stderr: THE WORLD IS A VAMPIRE.")
+}
