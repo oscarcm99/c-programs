ae67b0b6f5d687d0d031109f04bfa5a5223d271a
Oscar Calderon Moran <gorri0302@gmail.com>
2017-11-21 10:35:16 +0100

    name_int_hxd

---
+++ 2017/02_string/name.cpp
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+        char nombre[256];
+        printf("Dime tu nombre cabron: ");
+        scanf(" %s", nombre);
+        printf("Hola, %s.\n", nombre);
+
+
+	return EXIT_SUCCESS;
+}
---
+++ 2017/02_string/scan_set.cpp
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+        char nombre[16];
+        int num;
+        char hxdec[16];
+
+        /*ENTRADA DE DATOS*/
+        printf("Entero: ");
+        scanf(" %i", &num);
+        scanf(" %s", nombre);
+
+        /*SALIDA DE DATOS*/
+        printf("[%i] => %s.\n", num, nombre);
+
+        /*ENTRADA DE DATOS*/
+        printf("Hexadecimal: ");
+        scanf(" %[0123456789abcdefABCDEF]", hxdec);
+
+        /*SALIDA DE DATOS*/
+        printf("0x%sh\n", hxdec);
+	return EXIT_SUCCESS;
+}
