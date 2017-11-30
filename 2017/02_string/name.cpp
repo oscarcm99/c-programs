006fa7d7be533dd4a7eaab5561ddfe1b54d8e9d5
Oscar Calderon Moran <gorri0302@gmail.com>
2017-11-30 10:28:29 +0100

    Name

--- 2017/02_string/name.cpp
+++ 2017/02_string/name.cpp
@@ -1,12 +1,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#define N 0xFF
 int main(){
-        char nombre[256];
-        printf("Dime tu nombre cabron: ");
-        scanf(" %s", nombre);
-        printf("Hola, %s.\n", nombre);
+        char nombre[N],
+             apellidos[N],
+             dni[N],
+             direccion[N];
+        int  numero;
+        /*Entrada de datos*/
+        printf("Dime tu nombre: ");
+        scanf(" %[^\n]", nombre);
+        printf("Dime tus apellidos: ");
+        scanf(" %[^\n]", apellidos);
+        printf("Dime tu DNI: ");
+        scanf(" %[^\n]", dni);
+        printf("Dime tu direccion: ");
+        scanf(" %[^\n]", direccion);
+        printf("En que numero vives: ");
+        scanf(" %i", &numero);
+        /*Salida de datos*/
+        printf("Hola %s %s, has quedado registrado en nuestra BBDD con el DNI %s y tu direccion es %s numero %i\n", nombre, apellidos, dni, direccion, numero);
 
 
 	return EXIT_SUCCESS;
