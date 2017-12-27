b4cbfbda45c42ba612e6d601dc5b58eba0cd9ce0
oscar2710 <oscarcm99>
2017-12-27 23:48:31 +0100

    Basic calculator

--- 2017/projects/cal.cpp
+++ 2017/projects/cal.cpp
@@ -1,10 +1,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#define SUMA 1
+#define RESTA 2
+#define MULTIPLICACION 3
+#define DIVISION 4
+#define PORCENTAJE 5
 int main(){
     unsigned o;
-    double n, 
+    double p,
+           n, 
            m;
     system("toilet --gay  Calculadora");
     printf( "\t¿Que opción quieres realizar?\n"
@@ -12,38 +17,49 @@ int main(){
             "\t 2 Resta.\n"
             "\t 3 Multiplicación.\n"
             "\t 4 División.\n"
+            "\t 5 Porcentajes.\n"
             "\n"
             "\t Pulse la tecla que desee: ");
-            scanf(" %u", &o);
+    scanf(" %u", &o);
     switch(o){
-        case 1:
+        case SUMA:
             printf("\tDime el primer número: ");
             scanf(" %lf", &n);
             printf("\tDime el segundo número: ");
             scanf(" %lf", &m);
             printf("\tEl resultado es =  %.3lf\n",n + m );
             break;
-         case 2:
+        case RESTA:
             printf("\tDime el primer número: ");
             scanf(" %lf", &n);
             printf("\tDime el segundo número: ");
             scanf(" %lf", &m);
             printf("\tEl resultado es =  %.3lf\n",n - m );
             break; 
-         case 3:
+        case MULTIPLICACION:
             printf("\tDime el primer número: ");
             scanf(" %lf", &n);
             printf("\tDime el segundo número: ");
             scanf(" %lf", &m);
             printf("El resultado es =  %.3lf\n",n * m );
             break;
-         case 4:
+        case DIVISION:
             printf("\tDime el primer número: ");
             scanf(" %lf", &n);
             printf("\tDime el segundo número: ");
             scanf(" %lf", &m);
             printf("\tEl resultado es =  %.3lf\n",n / m );
             break;
+        case PORCENTAJE:
+            printf("\tDime un numero: ");
+            scanf(" %lf", &n);
+            printf("\tQue porcentaje deseas aplicar: ");
+            scanf(" %lf", &p);
+            printf("\tEl %.2lf por ciento de %.2lf es = %.3lf\n", p, n, n * p / 100 );
+            break;
+        default:
+            printf("\tERROR\n");
+            break;
     }
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
