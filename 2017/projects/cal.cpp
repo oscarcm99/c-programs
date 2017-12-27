074a06ef1f1222e9c8accb3ade04381e1f8b1499
oscar2710 <oscarcm99>
2017-12-28 00:24:04 +0100

    Basic Calculator

--- 2017/projects/cal.cpp
+++ 2017/projects/cal.cpp
@@ -1,23 +1,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define SUMA 1
 #define RESTA 2
 #define MULTIPLICACION 3
 #define DIVISION 4
 #define PORCENTAJE 5
+#define RAIZ 6
+#define RESTO 7
 int main(){
     unsigned o;
+    int a, 
+        b, 
+        c;
     double p,
            n, 
            m;
     system("toilet --gay  Calculadora");
+    printf("\t☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭☭\n");
     printf( "\t¿Que opción quieres realizar?\n"
             "\t 1 Suma.\n"
             "\t 2 Resta.\n"
             "\t 3 Multiplicación.\n"
             "\t 4 División.\n"
             "\t 5 Porcentajes.\n"
+            "\t 6 Raíces.\n"
+            "\t 7 Resto.\n"
             "\n"
             "\t Pulse la tecla que desee: ");
     scanf(" %u", &o);
@@ -27,35 +36,54 @@ int main(){
             scanf(" %lf", &n);
             printf("\tDime el segundo número: ");
             scanf(" %lf", &m);
-            printf("\tEl resultado es =  %.3lf\n",n + m );
+            p = n + m;
+            printf("\tEl resultado es =  %.3lf\n",p);
             break;
         case RESTA:
             printf("\tDime el primer número: ");
             scanf(" %lf", &n);
             printf("\tDime el segundo número: ");
             scanf(" %lf", &m);
-            printf("\tEl resultado es =  %.3lf\n",n - m );
+            p = n - m;
+            printf("\tEl resultado es =  %.3lf\n",p);
             break; 
         case MULTIPLICACION:
             printf("\tDime el primer número: ");
             scanf(" %lf", &n);
             printf("\tDime el segundo número: ");
             scanf(" %lf", &m);
-            printf("El resultado es =  %.3lf\n",n * m );
+            p = n * m;
+            printf("El resultado es =  %.3lf\n",p);
             break;
         case DIVISION:
             printf("\tDime el primer número: ");
             scanf(" %lf", &n);
             printf("\tDime el segundo número: ");
             scanf(" %lf", &m);
-            printf("\tEl resultado es =  %.3lf\n",n / m );
+            p = n / m;
+            printf("\tEl resultado es =  %.3lf\n",p);
             break;
         case PORCENTAJE:
             printf("\tDime un numero: ");
             scanf(" %lf", &n);
             printf("\tQue porcentaje deseas aplicar: ");
-            scanf(" %lf", &p);
-            printf("\tEl %.2lf por ciento de %.2lf es = %.3lf\n", p, n, n * p / 100 );
+            scanf(" %lf", &m);
+            p = n * m / 100;
+            printf("\tEl %.2lf por ciento de %.2lf es = %.3lf\n", m, n, p);
+            break;
+        case RAIZ:
+            printf("\tDime el número para calcular la raíz cuadrada: ");
+            scanf(" %lf", &n);
+            p = sqrt (n);
+            printf("\tLa raíz cuadrada de %.2lf es = %.2lf\n", n, p);
+            break;
+        case RESTO:
+            printf("\tDime el dividendo: ");
+            scanf(" %i", &a);
+            printf("\tDime el divisor: ");
+            scanf(" %i", &b);
+            c = a % b;
+            printf("\tEl resto es = %i\n", c);
             break;
         default:
             printf("\tERROR\n");
