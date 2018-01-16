b28c8287c95e55e1c164dbfc1fc4e335ca174bb1
oscar2710 <oscarcm99>
2018-01-16 20:43:59 +0100

    Diagonal

---
+++ 2017/09_iterativas/diagonal.cpp
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    for(int a = 0; a<1; a++){
+        printf("*\n");
+        printf("  *\n");
+        printf("    *\n");
+        printf("      *\n");
+        printf("        *\n");
+        printf("          *\n");
+    }
+ 	return EXIT_SUCCESS;
+}
