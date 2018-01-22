5460c1401bc2c9b7735f063c1d8c283d0be552dc
Oscar Calderon Moran <gorri0302@gmail.com>
2018-01-22 12:08:24 +0100

    WWV

--- 2017/09_iterativas/diago.cpp
+++ 2017/09_iterativas/diago.cpp
@@ -4,7 +4,7 @@
 #define N 3
 int main(){
     for(int f=0; f<N; f++){
-        for(int t=0; t<5; t++)
+        for(int t=0; t<10; t++)
             for(int c = 0; c<N; c++)
                 if(f==c && t%2==0)
                    printf("* ");
