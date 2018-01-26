d5c2f2aac18fa124b7c851ce889d6c8a58c90159
oscar2710 <oscarcm99>
2018-01-26 22:55:34 +0100

    Sistemas de ecuaciones

--- 2017/10_arrativas/sistema.cpp
+++ 2017/10_arrativas/sistema.cpp
@@ -12,24 +12,42 @@ void sist(double a[N][N]){
     }
 }
 int main(){
-    double a[L][N]={
-        {2,3,4},
-        {1,2,1}
-    },
+    double a[L][N],
            coef[L],
            k;
            int celda=0;
-           sist(a);
-           for(int n=0; n<L; n++){
-               coef[celda++] = k = a[n][n];
-               for(int col=0; col<N; col++)
-                   a[n][col] /= k;
-               for(int f=n+1; f<L; f++){
-                   k = a [f][n];
-                   for(int col=0; col<N; col++)
-                       a[f][col] -= (a[n][col]*k);
-               }
-               sist(a);
-           }
+           double x,
+                  y;
 
+    printf("Dime lo que multiplica a x: ");
+    scanf(" %lf", &a[0][0]);
+    printf("Dime lo que multiplica a y: ");
+    scanf(" %lf", &a[0][1]);
+    printf("Dime el resultado de la primera ecuación: ");
+    scanf(" %lf", &a[0][2]);
+    printf("Dime lo que multiplica a x: ");
+    scanf(" %lf", &a[1][0]);
+    printf("Dime lo que multiplica a y: ");
+    scanf(" %lf", &a[1][1]);
+    printf("Dime el resultado de la segunda ecuación: ");
+    scanf(" %lf", &a[1][2]);
+
+
+
+    sist(a);
+    for(int n=0; n<L; n++){
+        coef[celda++] = k = a[n][n];
+        for(int col=0; col<N; col++)
+            a[n][col] /= k;
+        for(int f=n+1; f<L; f++){
+            k = a [f][n];
+            for(int col=0; col<N; col++)
+                a[f][col] -= (a[n][col]*k);
+        }
+        sist(a);
+        printf("\n");
+    }
+    y= a[1][2] / a[1][1];
+    x = (a[0][2] - y *a[0][1])/a[0][0];
+    printf("x = %.2lf y = %.2lf\n", x,y);
 }
