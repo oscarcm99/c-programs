7378f0d71e831c254714c14ed750be586e893335
oscarcm99 <gorri0302@gmail.com>
2018-02-03 10:55:23 +0100

    Mul

--- 2017/10_arrativas/multiplicacion.cpp
+++ 2017/10_arrativas/multiplicacion.cpp
@@ -1,42 +1,26 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #define N 2
-void matrizA(int a[N][N] ){
-    for(int f=0; f<N; f++){
-        for(int c=0; c<N; c++)
-            printf("%4i", a[f][c]);
-        printf("\n");
-    }
-}
-void matrizB(int b[N][N]){
-    for(int f=0; f<N; f++){
-        for(int c=0; c<N; c++)
-            printf("%4i", b[f][c]);
-        printf("\n");
-     }
-}
 int main(){
-    int a[N][N]={
-        {2,5},
-        {3,7}
-    };
-    int b[N][N]={
-        {4,6},
-        {2,9}
-    };
-    int r[N][N];
-    int i=0,j=0;
-    int p;
-    matrizA(a);
-    printf("\n");
-    matrizB(b);
-
-    for(int f=0; f<N; f++){
-        for(int c=0; c<N; c++)
-            for(int k=0; k<N; k++)
-                r[i][j]+= a[i][k]*b[k][j];
-        printf("%i", r[i][j]);
-    }
-    return EXIT_SUCCESS;
+	int a[N][N]={
+		{2,5},
+		{4,2}
+	};
+	int b[N][N]={
+		{1,6},
+		{5,8}
+	};
+	int f;
+	int r[N][N];
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			f=0;
+			for(int k=0; k<N; k++)
+				f+=a[i][k]*b[k][j];
+			r[i][j]= f;
+			printf("%4i",r[i][j]);
+		}
+		printf("\n");
+	}
+	return EXIT_SUCCESS;
 }
