e653d6681b5569fa4681086a5d6628f7da6d0eea
oscarcm99 <gorri0302@gmail.com>
2018-02-22 19:15:55 +0100

    Vocales

--- 2017/13_lista/vocal.cpp
+++ 2017/13_lista/vocal.cpp
@@ -4,18 +4,18 @@
 //pedir una vocal al usuario
 //que en la frase esa vocal introducida cambie a todas las vocales de la frase
 int main(){
-    char frase[] = "Mi moto alpina derrapante";
-    char *letra = frase;
-    char guardado;
-    printf("Dame una vocal: ");
-    scanf(" %[aeiou]", &guardado);
-    while(*letra != '\0'){
-        if(*letra=='a' || *letra=='e' || *letra=='i' || *letra=='o' || *letra=='u'){
-            *letra = guardado;
-            letra++;
-        }
-
-        printf("%c\n", *++letra);
-    }
-    return EXIT_SUCCESS;
+	char frase[] = "Mi moto alpina derrapante";
+	char *letra = frase;
+	char guardado;
+	char gu;
+	printf("Dame una vocal: ");
+	scanf(" %c", &guardado);
+	while(*letra != '\0'){
+		if(*letra=='a' || *letra == 'e' || *letra == 'i' || *letra == 'o' || *letra == 'u')
+			*letra = guardado;
+		printf("%c\n", *letra);
+		letra++;
+	}	
+	printf("\n");
+	return EXIT_SUCCESS;
 }
