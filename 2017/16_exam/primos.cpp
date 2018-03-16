05c1d1b0dd576efe9c114fa81f5707ee74864b28
Oscar Calderon Moran <gorri0302@gmail.com>
2018-03-16 13:28:33 +0100

    primos

--- 2017/16_exam/primos.cpp
+++ 2017/16_exam/primos.cpp
@@ -4,9 +4,9 @@
 #define N 100
 
 
-bool es_primo(int n){
-    for(int d=2; d<N; d++)
-        if(n % d == 0)
+bool es_primo(int numero){
+    for(int divisor=2; divisor<numero; divisor++)
+        if(numero % divisor == 0)
             return false;
     return true;
 }
@@ -15,8 +15,13 @@ int main(){
 
     int primo[N];
 
-for(int np = 0, pp=2; np<N; pp++)
-    if(es_primo(pp))
-        es_primo[np++] = pp;
+    for(int np = 0, pp=2; np<N; pp++)
+        if(es_primo(pp))
+            primo[np++] = pp;
+
+    for(int f=0; f<N; f++)
+        printf(" %i ", primo[f]);
+
+    printf("\n");
     return EXIT_SUCCESS;
 }
