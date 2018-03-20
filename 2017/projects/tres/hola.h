0abbcb41f7b4b7ee96798c9b25c8db60b0138102
Oscar Calderon Moran <gorri0302@gmail.com>
2018-03-20 11:15:11 +0100

    3 en raya mejorado en ficheros

---
+++ 2017/projects/tres/hola.h
@@ -0,0 +1,11 @@
+#ifndef __HOLA_H__
+#define __HOLA_H__
+#include "comun.h"
+#ifdef __cpp
+extern "C"{
+#endif
+void loop(char a[N][N]);
+#ifdef __cpp
+}
+#endif
+#endif
---
+++ 2017/projects/tres/tresraya.cpp
@@ -0,0 +1,12 @@
+//Dibujar una matriz de 3 x 3 para jugar al tres en raya
+//Pedir a dos usuarios O o X para jugar al juego del que vamos a jugar con coordenadas
+#include <stdio.h>
+#include <stdlib.h>
+#include "comun.h"
+#include "hola.h"
+
+int main(){
+    char a[N][N];
+    loop(a);//llama a todas las funciones y hace refresco de la pantalla
+    return EXIT_SUCCESS;
+}
