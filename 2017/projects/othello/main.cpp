1b5759558c1ab73feac4dc079d61219ac384977e
Oscar Calderon Moran <gorri0302@gmail.com>
2018-03-20 12:07:52 +0100

    othello matriz

--- 2017/projects/othello/main.cpp
+++ 2017/projects/othello/main.cpp
@@ -11,16 +11,21 @@ void introducir(char a[N][N]){
     }
 }
 void pintar_matriz(char a[N][N]){
+    int num = 0;
+    printf("\n\t______________________________________________________________\n");
+    printf("\n");
     for(int f=0; f<N; f++){
         for(int c=0; c<N; c++)
-            if(c<7)
-                printf(" %c |", a[f][c]);
-            else
-                printf(" %c ", a[f][c]);
-        if(f<7)
-            printf("\n--------------------------------\n");
+            if(c<8)
+                printf("\t  %c   |", a[f][c]);
+        if(f<8)
+            printf("\n\t______________________________________________________________\n");
         printf("\n");
     }
+    a[3][3] ='O';
+    a[3][4] ='X';
+    a[4][3] ='X';
+    a[4][4] ='O';
 }
 int main(){
         char a[N][N];
