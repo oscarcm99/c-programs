d6d9572620d1fac730013236a0d29bfd80e8e713
Oscar Calderon Moran <gorri0302@gmail.com>
2018-03-21 13:35:44 +0100

    Reversi game

--- 2017/projects/othello/main.cpp
+++ 2017/projects/othello/main.cpp
@@ -1,10 +1,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #define N 8
+#define J1 'X'
+#define J2 'O'
 void inicio(){
     system("clear");
-    system()
+    system("\ttoilet -fpagga REVERSI");
 }
 void introducir(char a[N][N]){
     char relleno = '\0';
@@ -25,18 +28,40 @@ void pintar_matriz(char a[N][N]){
             printf("\n\t______________________________________________________________\n");
         printf("\n");
     }
-    a[3][3] ='O';
-    a[3][4] ='X';
-    a[4][3] ='X';
-    a[4][4] ='O';
 }
-void usuario(){
-    int f,c;
-    printf("Mete las coordenadas: ");
+void usuario(char a[N][N]){
+    int f,c,k;
+    do{
+        printf("Mete las coordenadas: ");
+        scanf(" %i, %i", &f,&c);
+        for(int fila=0; fila<N;fila++){
+            k=0;
+            for(int col=0; col<N; col++)
+                if(a[f][c] == 'X' || a[f][c] == 'O'){
+                    k=1;
+                    printf("La casilla está ocupada.\n");
+                }
+        }
+    }while(k == 1);
+    a[f][c] = 'X';
+}
+void cpu(char a[N][N]){
+    int f,c,k;
+    srand(time(NULL));
+        do{
+            f = rand() % N;
+            c = rand() % N;
+            k = 0;
+            if(a[f][c] == 'X' || a[f][c] == 'O')
+                k=1;
+        }while(k == 1);
+    a[f][c] = 'O';
 }
 int main(){
-        char a[N][N];
-        introducir(a);
-        pintar_matriz(a);
-	return EXIT_SUCCESS;
+    char a[N][N];
+    int fila,columna;
+    inicio();
+    introducir(a);
+    pintar_matriz(a);
+    return EXIT_SUCCESS;
 }
