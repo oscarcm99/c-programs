0191b3137d3d38bfbab511163a751864009094d0
Oscar Calderon Moran <gorri0302@gmail.com>
2018-04-26 13:42:50 +0200

    Pilas

---
+++ 2017/20_pila/pila.cpp
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#define N 0x100
+#define DEBUG(...) printf(__VA_ARGS__);
+
+typedef struct{
+    int data[N];
+    int cima;
+}Pila;
+
+void push(int numero, Pila *posicion){
+    DEBUG("-> %5i\n", numero);
+    posicion->data[posicion->cima] = numero;
+    posicion->cima++;
+}
+int pop(Pila *posicion){
+    posicion->cima--;
+    DEBUG("<- %5i\n", posicion->data[posicion->cima]);
+    return posicion->data[posicion->cima];
+}
+int main(){
+    Pila posicion;
+    posicion.cima = 0;
+    push(5,&posicion);
+    push(2,&posicion);
+    push(3,&posicion);
+    pop(&posicion);
+    pop(&posicion);
+    return EXIT_SUCCESS;
+}
