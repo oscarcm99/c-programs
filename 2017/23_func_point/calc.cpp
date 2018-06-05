7057ab07d57e19ad844dd378e88fc7b07964435c
oscarcm99 <gorri0302@gmail.com>
2018-06-05 22:12:14 +0200

    fun

--- 2017/23_func_point/calc.cpp
+++ 2017/23_func_point/calc.cpp
@@ -2,26 +2,17 @@
 #include <stdlib.h>
 
 #define N 16
-#define MAX 10
-#define DEBUG(...) printf(__VA_ARGS__);
-int  error = 0;
-const char *mssg[] = {
-	"Todo OK.",
-	"Pila vacía.",
-	"Pila llena."
-};
-
-typedef struct {
+#define MAX 0x100
+int error = 0;
+typedef struct{
 	char nombre[N];
-	double (*op)(double, double);
-} Operacion;
+	double (*op)(double,double);
+}Operacion;
 
-typedef struct {
+typedef struct{
 	int data[MAX];
 	int cima;
-} Pila;
-
-
+}Pila;
 enum {
 	suma,
 	resta,
@@ -29,80 +20,78 @@ enum {
 	division,
 	OPERA
 };
-
 bool push(int dato, Pila *pila){
-	if (pila->cima >= MAX){
-		error = 2;
+	if(pila->cima>=MAX){
+		error=2;
 		return false;
 	}
-	error = 0;
+	error =0;
 	pila->data[pila->cima++] = dato;
 	return true;
 }
 
-int pop(Pila *pila) {
-	if (pila->cima == 0){
-		error = 1;
-		return 0;
+int pop(Pila *pila){
+	if(pila->cima == 0){
+	error = 1;
+	return 0;
 	}
-	error = 0;
-	pila->cima--;
-	DEBUG("T_T %i\n", pila->data[pila->cima]);
-	return pila->data[pila->cima];
+	error  = 0;
+	return pila->data[--pila->cima];
 }
 
-double sum(double op1, double op2) { return op1 + op2; }
-double res(double op1, double op2) { return op1 - op2; }
-double mul(double op1, double op2) { return op1 * op2; }
-double div(double op1, double op2) { return op1 / op2; }
+double sum(double op1, double op2){return op1+op2;}
+double res(double op1, double op2){return op1-op2;}
+double mul(double op1, double op2){return op1*op2;}
+double div(double op1, double op2){return op1/op2;}
 
 int main(){
-
-	Pila op, datos;
+        Pila op, datos;
+	op.cima = 0;
+	datos.cima = 0;
 	double op1, op2;
-	char opera;
-	int veces;
-	bool ver = true;
-	Operacion catalogo[] = {
-		{"suma",  &sum},
-		{"resta", &res},
-		{"multiplacion", &mul},
+	char signo;
+	bool fin = false;
+	Operacion catalogo[]{
+		{"suma",&sum},
+		{"resta",&res},
+		{"multiplicacion", &mul},
 		{"division", &div}
 	};
-	printf("Cuantas operaciones vas a hacer: ");
-	scanf(" %i", &veces);
-	while(veces>0){
-		printf("Operacion: ");
-		scanf(" %lf %c %lf", &op1, &opera, &op2);
-		ver = push(op2, &datos);
-		ver = push(op1, &datos);
-		if(ver == true){
-			switch(opera) {
-				case '+':
-					push(suma, &op);
-					break;
-				case '-':
-					push(resta, &op);
-					break;
-				case '*':
-					push(multi, &op);
-					break;
-				case '/':
-					push(division, &op);
-					break;
-				default:
-					printf("aaaaaaaaaaaaaaaaaa\n");
-					break;
+	printf("Introduce operacion: ");
+	scanf(" %lf",&op1);
+	push(op1,&datos);
 
-			}
+	do{
+		scanf(" %c %lf", &signo, &op1);
+		push(op1,&datos);
+		switch(signo){
+			case '+':
+				push(suma,&op);
+				break;
+			case '-':
+				push(resta,&op);
+			case '/':
+				push(division,&op);
+				break;
+			case '*':
+				push(multi,&op);
+				break;
 		}
-		veces--;
+		char c = getchar();
+		if(c == '\n')
+			fin = true;
+		else
+			ungetc(c,stdin);
+	}while(!fin);
+
+	while(op.cima != 0){
+	double(*operacion)(double,double);
+	op1 = pop(&datos);
+	op2 = pop(&datos);
+	operacion = catalogo[pop(&op)].op;
+	int res = (operacion)(op1,op2);
+	push(res,&datos);
 	}
-	for(int i=0; i<MAX; i++){
-		int num = pop(&op);
-		double res = catalogo[num].op((double) pop(&datos), (double) pop(&datos) );
-		printf("%s\n", catalogo[num].nombre);
-		printf("Resultado: %.2lf\n", res);
-	}
+	printf("Resultado: %.i\n",pop(&datos));
 	return EXIT_SUCCESS;
 }
